be3ea84d425324ccf4f00c982fd6b188f1391352
Brenn0219 <brennoaugusto2109@gmail.com>
2024-03-04 01:30:01 -0300

    refactor: database configuration

---
+++ Contexts/AppDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using crud.Models;
+
+namespace crud.Contexts 
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
