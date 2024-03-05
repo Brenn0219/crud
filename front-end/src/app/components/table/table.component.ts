b45bd000665804a2306355be1e1c55435a8f2f1e
Brenn0219 <brennoaugusto2109@gmail.com>
2024-03-05 13:54:50 -0300

    feat(front-end): update and delete

--- front-end/src/app/components/table/table.component.ts
+++ front-end/src/app/components/table/table.component.ts
@@ -9,6 +9,7 @@ import { User } from '../../User';
 })
 export class TableComponent {
   users: User[] = []
+  user!: User
 
   constructor(private userService: UserService) {
     this.getUsers();
@@ -16,5 +17,15 @@ export class TableComponent {
 
   getUsers(): void {
     this.userService.getAllUsers().subscribe((users) => (this.users = users));
+  }
+
+  updateUser(user: User): void {
+    this.userService.updateUser(user).subscribe();
+  }
+
+  deleteUser(id: number): void {
+    this.userService.deleteUser(id).subscribe(() => {
+      this.users = this.users.filter(u => u.id !== id);
+    });
   }
 }
