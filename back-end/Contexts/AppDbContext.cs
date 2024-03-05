c82df61d304bf57277cc7e824e4c85614fb2618c
Brenn0219 <brennoaugusto2109@gmail.com>
2024-03-05 11:18:41 -0300

    fix: user table

--- back-end/Contexts/AppDbContext.cs
+++ back-end/Contexts/AppDbContext.cs
@@ -11,5 +11,17 @@ namespace crud.Contexts
         }
 
         public DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            
+            builder.Entity<User>().ToTable("User");
+            builder.Entity<User>().HasKey(u => u.Id);
+            builder.Entity<User>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+            builder.Entity<User>().Property(u => u.Name).IsRequired().HasMaxLength(30);
+            builder.Entity<User>().Property(u => u.Email).IsRequired().HasMaxLength(50);
+            builder.Entity<User>().Property(u => u.CEP).IsRequired();
+        }
     }
 }
