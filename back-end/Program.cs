ba5971506efd9a2557d9e1c28af12c21d628378b
Brenn0219 <brennoaugusto2109@gmail.com>
2024-03-05 00:42:36 -0300

    feat: cors

--- back-end/Program.cs
+++ back-end/Program.cs
@@ -24,6 +24,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(options =>
+{
+    options.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader();
+});
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
